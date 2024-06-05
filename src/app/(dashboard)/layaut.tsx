f7052d3e63ccb2d295895e20f50a4ace7e147628
JhosepDevC <devj88717@gmail.com>
2024-06-05 18:04:34 -0400

    feat: adicion de las configuraciones iniciales

---
+++ src/app/(dashboard)/layaut.tsx
@@ -0,0 +1,13 @@
+
+export default function DashboardLayout({children}:{children: React.ReactNode;}) {
+  return (
+    <>
+        <h1>Dashboar layaut</h1>
+        <h2>ladasf</h2>
+        <main>
+            {children}
+        </main>
+    </>
+    
+  );
+}
